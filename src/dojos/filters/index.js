d5672ffc8f62a013a2b116ff2efa552195af05a5
Daniel Brierton <daniel@brierton.me>
2017-05-25 14:28:14 +0100

    [#145869777] Added Private/Public indicator for dojos list

---
+++ src/dojos/filters/index.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import cdDojoPrivate from './cd-dojo-private';
+
+Vue.filter('cd-dojo-private', cdDojoPrivate);
---
+++ test/unit/specs/dojos/filters/cd-dojo-private.spec.js
@@ -0,0 +1,8 @@
+import cdDojoPrivate from '@/dojos/filters/cd-dojo-private';
+
+define('Dojo private filter', () => {
+  it('should convert private flag', () => {
+    expect(cdDojoPrivate(1)).to.be.equal('Private');
+    expect(cdDojoPrivate(0)).to.be.equal('Public');
+  });
+});
