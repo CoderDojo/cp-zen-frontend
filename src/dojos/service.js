97e450432f140cc923f0712de1ad3c0e3812d3a3
Daniel Brierton <daniel@brierton.me>
2017-05-26 17:01:39 +0100

    Fixed radius for search by lat lon[#145993173]

--- src/dojos/service.js
+++ src/dojos/service.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 
+const radius = 50000;
+
 const DojosService = {
   getDojos: () => Vue.http.post(`${Vue.config.apiBase}/dojos`),
   getDojosByLatLong: (lat, lon) => Vue.http.post(`${Vue.config.apiBase}/dojos/search-bounding-box`, {
     query: {
       lat,
       lon,
-      radius: 50,
+      radius,
     },
   }),
 };
