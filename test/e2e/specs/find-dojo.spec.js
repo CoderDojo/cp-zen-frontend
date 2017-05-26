e4494b9ec1f4e35f89ae4cc402bb081bcda7e070
Daniel Brierton <daniel@brierton.me>
2017-05-26 16:22:55 +0100

    Added pause in e2e tests [#145993173]

--- test/e2e/specs/find-dojo.spec.js
+++ test/e2e/specs/find-dojo.spec.js
@@ -3,6 +3,7 @@ const DojoPage = require('../page-objects/dojo-page');
 describe('Find Dojo page', () => {
   it('should have a Get current location button', () => {
     DojoPage.open();
+    browser.pause(2000);
     expect(DojoPage.detectLocationButton.isVisible()).to.equal(true);
   });
 });
