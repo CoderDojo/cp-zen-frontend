716cbdcfed2579da873efb0af3fe516ebefb0f20
Daniel Brierton <daniel@brierton.me>
2017-06-02 09:43:36 +0100

    Added DOB validation for Event details page[#146278973]

--- test/unit/specs/events/cd-event-details.spec.js
+++ test/unit/specs/events/cd-event-details.spec.js
@@ -1,32 +1,43 @@
 import vueUnitHelper from 'vue-unit-helper';
+import { extend } from 'lodash';
 import EventDetails from '!!vue-loader?inject!@/events/cd-event-details';
 
 describe('Event details', () => {
-  const sandbox = sinon.sandbox.create();
-  const MockEventService = {
-    loadEvent: sandbox.stub(),
+  let vm;
+  let sandbox;
+  let MockEventService;
+  let MockStoreService;
+  let EventDetailsWithMocks;
+  const mockEventData = {
+    key: 'val',
   };
-  const MockStoreService = {
-    save: sinon.stub(),
-  };
-  const EventDetailsWithMocks = EventDetails({
-    './service': MockEventService,
-    '@/store/store-service': MockStoreService,
-  });
 
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+    MockStoreService = {
+      save: sandbox.stub(),
+    };
+    MockEventService = {
+      loadEvent: sandbox.stub(),
+    };
+    EventDetailsWithMocks = EventDetails({
+      './service': MockEventService,
+      '@/store/store-service': MockStoreService,
+    });
+    vm = vueUnitHelper(EventDetailsWithMocks);
+    vm.eventId = 1;
+  });
   afterEach(() => {
     sandbox.restore();
   });
 
   it('should get the event details for the given eventId', (done) => {
     // ARRANGE
-    const mockEventData = {
-      key: 'val',
-    };
-    MockEventService.loadEvent.withArgs(1).returns(Promise.resolve({ body: mockEventData }));
-
-    const vm = vueUnitHelper(EventDetailsWithMocks);
-    vm.eventId = 1;
+    MockEventService.loadEvent.withArgs(1).returns(Promise.resolve(
+      {
+        body: mockEventData,
+      },
+    ));
 
     // ACT
     vm.loadEvent();
@@ -39,15 +50,9 @@ describe('Event details', () => {
   });
 
   it('should save selected event and go to next page', () => {
-    const mockEventData = {
-      key: 'val',
-    };
-    const vm = vueUnitHelper(EventDetailsWithMocks);
-    vm.eventId = 1;
-
     const data = {
       $router: {
-        push: sinon.spy(),
+        push: sandbox.spy(),
       },
       eventDetails: mockEventData,
     };
@@ -58,5 +63,34 @@ describe('Event details', () => {
 
     expect(data.$router.push).to.be.calledOnce;
     expect(data.$router.push).to.have.been.calledWith(`/events/${data.eventId}/sessions`);
+  });
+
+  it('should validate date of birth', () => {
+    const data = {
+      $router: {
+        push: sandbox.spy(),
+      },
+      eventDetails: mockEventData,
+      day: '27',
+      month: '03',
+      year: '2017',
+      isDobUnderage: false,
+    };
+
+    expect(vm.isDobUnderage).to.equal(false);
+
+    extend(vm, data);
+
+    vm.next();
+    expect(vm.isDobUnderage).to.equal(true);
+
+    vm.year = '1980';
+    vm.next();
+    expect(vm.isDobUnderage).to.equal(false);
+    expect(MockStoreService.save).to.be.calledOnce;
+    expect(MockStoreService.save).to.have.been.calledWith('selected-event', mockEventData);
+
+    expect(vm.$router.push).to.be.calledOnce;
+    expect(vm.$router.push).to.have.been.calledWith(`/events/${vm.eventId}/sessions`);
   });
 });
