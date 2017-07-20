d6d48221cad82014b2aa14f9300f8c325b688add
Daniel Brierton <daniel@brierton.me>
2017-07-20 17:34:41 +0100

    [#147740351] Implemented OpenGraph tags for dojos

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -56,12 +56,27 @@ var webpackConfig = merge(baseWebpackConfig, {
       filename: process.env.NODE_ENV === 'testing'
         ? 'index.html'
         : config.build.index,
-      template: 'index.html',
+      template: 'index.ejs',
       inject: true,
       minify: {
         removeComments: true,
-        collapseWhitespace: true,
-        removeAttributeQuotes: true
+        collapseWhitespace: true
+        // more options:
+        // https://github.com/kangax/html-minifier#options-quick-reference
+      },
+      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+      chunksSortMode: 'dependency'
+    }),
+    // generate dist opengraph.hbs with correct asset hash for caching.
+    // Used when requesting pages which should have OpenGraph meta tags on request
+    new HtmlWebpackPlugin({
+      filename: 'opengraph.hbs',
+      template: 'index.ejs',
+      openGraph: true,
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
         // more options:
         // https://github.com/kangax/html-minifier#options-quick-reference
       },
