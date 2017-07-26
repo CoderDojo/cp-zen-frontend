c7d13f1c0b3050fe3d7e0b49b380d6665df5d39d
Daniel Brierton <daniel@brierton.me>
2017-07-26 11:38:53 +0100

    [#149098629] Add Google Analytics to new pages (navigation)

--- config/e2e.env.js
+++ config/e2e.env.js
@@ -3,5 +3,6 @@ var devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
   NODE_ENV: '"e2e"',
-  API_SERVER: '"http://localhost:8080"'
+  API_SERVER: '"http://localhost:8080"',
+  GOOGLE_ANALYTICS_PROPERTY_ID: '"UA-25136319-8"'
 })
