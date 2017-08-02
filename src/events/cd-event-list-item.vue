7ad55a2f091ad443daa2f1120588b7d155d9ee17
Graham Bartley <grabartley@gmail.com>
2017-08-02 16:21:14 +0100

    [#149099333] Mary wants to not book into past events (#13)
    * [#149099333] Mary wants to not book into past events
    
    * [#149099333] Fixed timezone issue

--- src/events/cd-event-list-item.vue
+++ src/events/cd-event-list-item.vue
@@ -20,7 +20,7 @@
         </div>
       </div>
     </div>
-    <div v-if="canBook" class="cd-event-list-item__view-wrapper">
+    <div v-if="canBook && !isPastEvent" class="cd-event-list-item__view-wrapper">
       <div v-if="event.eventbriteId">
         <a :href="event.eventbriteUrl | cdUrlFormatter" target="_blank" class="btn btn-lg btn-primary cd-event-list-item__view">{{ $t('See Details and Book') }}</a>
       </div>
@@ -33,6 +33,7 @@
   </div>
 </template>
 <script>
+  import moment from 'moment';
   import cdDateFormatter from '@/common/filters/cd-date-formatter';
   import cdTimeFormatter from '@/common/filters/cd-time-formatter';
   import cdUrlFormatter from '@/common/filters/cd-url-formatter';
@@ -66,6 +67,12 @@
       },
       getSessionListForEvent() {
         return this.event.sessions.map(session => session.name).join(', ');
+      },
+      isPastEvent() {
+        const now = moment();
+        const eventStartTime = moment(this.event.dates[this.event.dates.length - 1].startTime);
+        eventStartTime.subtract(eventStartTime.utcOffset(), 'minutes');
+        return now.isAfter(eventStartTime);
       },
     },
     filters: {
