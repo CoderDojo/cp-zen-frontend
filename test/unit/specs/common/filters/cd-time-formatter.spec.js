f4d6048d74a619b80986aeb9f596161fd8e40218
Graham Bartley <grabartley@gmail.com>
2017-08-18 10:51:59 +0100

    Fix for Eventbrite times

--- test/unit/specs/common/filters/cd-time-formatter.spec.js
+++ test/unit/specs/common/filters/cd-time-formatter.spec.js
@@ -3,9 +3,9 @@ import cdTimeFormatter from '@/common/filters/cd-time-formatter';
 describe('Time formatting filter', () => {
   it('should format time using 12hr utc format', () => {
     expect(cdTimeFormatter('2017-06-17T16:00:00.000Z')).to.equal('4pm');
-    expect(cdTimeFormatter('2017-06-17T06:00:00.000Z')).to.equal('6am');
+    expect(cdTimeFormatter('2017-06-17T06:00:00.000')).to.equal('6am');
     expect(cdTimeFormatter('2017-06-17T23:45:00.000Z')).to.equal('11:45pm');
-    expect(cdTimeFormatter('2017-06-17T00:30:00.000Z')).to.equal('12:30am');
+    expect(cdTimeFormatter('2017-06-17T00:30:00.000')).to.equal('12:30am');
     expect(cdTimeFormatter('2017-06-17T00:')).to.equal('Invalid date');
   });
 });
