cb0e404cce621f20803932dfc704880f803f0254
campbs <suzannecampbell95@gmail.com>
2017-09-04 15:53:16 +0100

    [#148741643] Investigate making sure new pages scroll to top, not where scroll is on previous page. (#54)
    * [#148741643] Investigate making sure new pages scroll to top, not where scroll is on previous page.
    
    * [#148741643] Changed scroll to top function to allow for saved positions
    
    * [#148741643] Partial anchor support in the router
    
    * [#148741643] Put savedPosition before to.hash

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,16 @@ Vue.use(Router);
 
 export default new Router({
   mode: 'history',
+  scrollBehavior: (to, from, savedPosition) => {
+    if (savedPosition) {
+      return savedPosition;
+    } else if (to.hash) {
+      return {
+        selector: to.hash,
+      };
+    }
+    return { x: 0, y: 0 };
+  },
   routes: [
     {
       path: '/',
