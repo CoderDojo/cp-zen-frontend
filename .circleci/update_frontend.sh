fb7b8f10b60c63c027ea804f8e62edce523979e7
butlerx <butlerx@notthe.cloud>
2017-10-10 16:02:31 +0100

    use browser image

--- .circleci/update_frontend.sh
+++ .circleci/update_frontend.sh
@@ -2,19 +2,14 @@
 
 set -e
 
-if [ "$GIT_BRANCH" = "master" ]; then
-  BRANCH=latest
-elif [ "$GIT_BRANCH" = "staging" ]; then
-  BRANCH=staging
-else
-  exit 0
-fi
-
+GIT_BRANCH=$1
+TAG=$2
 git clone git@github.com:CoderDojo/cp-zen-platform.git
 cd cp-zen-platform || exit 1
 git config --global user.email "webteam@coderdojo.com"
 git config --global user.name "CoderDojo Foundation"
-yarn add cp-zen-frontend@"$BRANCH"
+git checkout "$GIT_BRANCH"
+yarn add cp-zen-frontend@"$TAG"
 git add .
 git commit -m "[ci] Update frontend"
 git push
