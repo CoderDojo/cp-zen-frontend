74c82f93fd104116e0d45941ca2d752f8ec27120
Cian Butler <butlerx@notthe.cloud>
2017-10-11 16:07:04 +0100

    Circle v2 (#66)
    * use circleci 2

--- config/e2e.env.js
+++
@@ -1,9 +0,0 @@
-var merge = require('webpack-merge')
-var devEnv = require('./dev.env')
-
-module.exports = merge(devEnv, {
-  NODE_ENV: '"e2e"',
-  API_SERVER: '"http://localhost:8080"',
-  GOOGLE_ANALYTICS_PROPERTY_ID: '"UA-25136319-8"',
-  GOOGLE_MAPS_API_KEY: '"AIzaSyC3xF9XV91bS2R14Gjmx3UQaKbGgAfHbE4"'
-})
