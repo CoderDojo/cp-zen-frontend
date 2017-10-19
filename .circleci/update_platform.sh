c8211ee1728c9feb22064a88b5199dea82cea156
Daniel Brierton <daniel@brierton.me>
2017-10-19 10:23:55 +0100

    Re-added (now working, hopefully) step to trigger cp-zen-platform build on deploy

---
+++ .circleci/update_platform.sh
@@ -0,0 +1,15 @@
+#!/user/bin/env bash
+set -e
+
+if [ -z "$2" ]; then
+  echo "Usage ./update_platform.sh [NPM_TAG] [BRANCH_TO_UPDATE]"
+  exit 1
+fi
+
+NPM_TAG=$1
+BRANCH_TO_UPDATE=$2
+
+curl -u $CIRCLE_TOKEN: \
+  -d build_parameters[CIRCLE_JOB]=update-frontend \
+  -d build_parameters[CP_ZEN_FRONTEND_NPM_TAG]=$NPM_TAG \
+  https://circleci.com/api/v1.1/project/github/CoderDojo/cp-zen-platform/tree/$BRANCH_TO_UPDATE
