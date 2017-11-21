8fe0254b91f457133d093641e28d0a1a201008a9
Wardormeur <wardormeur@gmail.com>
2017-11-21 13:55:29 +0000

    Add error message for invalid date on first step of FTBooking flow

--- src/events/cd-event-dob-verification.vue
+++ src/events/cd-event-dob-verification.vue
@@ -6,12 +6,24 @@
       <label class="cd-event-dob-verification__dob-input-label" for="dob">{{ $t('Enter your Date of Birth') }}</label>
       <div class="cd-event-dob-verification__dob-picker-wrapper">
         <vue-dob-picker v-model="date" select-class="form-control" id="dob" class="cd-event-dob-verification__dob"
-                        show-labels="false" month-format="short"
-                        :placeholders="[$t('Date'), $t('Month'), $t('Year')]"
-                        :proportions="[2, 2, 3]"></vue-dob-picker>
+          v-validate="'required'"
+          data-vv-name="parentDob"
+          data-vv-value-path="value"
+          data-vv-as="date of birth"
+          show-labels="false" month-format="short"
+          :placeholders="[$t('Date'), $t('Month'), $t('Year')]"
+          :proportions="[2, 2, 3]"></vue-dob-picker>
       </div>
-      <button @click="$router.back()" class="cd-event-dob-verification__cancel btn btn-primary">{{ $t('Cancel') }}</button>
-      <input type="submit" class="cd-event-dob-verification__verify btn btn-primary" :value="$t('Verify')">
+      <p class="cd-event-dob-verification__dob-info"><i class="fa fa-exclamation-circle"></i>
+          {{ $t('If you are a parent or a guardian booking tickets for a child, please still tell us your Date of Birth') }} </p>
+      <div class="cd-event-dob-verification__actions">
+        <button @click="$router.back()" class="cd-event-dob-verification__actions-cancel btn btn-primary">{{ $t('Cancel') }}</button>
+        <div class="cd-event-dob-verification__actions-block" :class="{ 'cd-event-dob-verification__actions-block-errors' : errors.has('parentDob:required') }">
+          <label class="cd-event-dob-verification__invalid-dob-error text-danger"
+            v-show="errors.has('parentDob:required')">{{ $t('Date of birth is required') }}</label>
+          <input type="submit" class="cd-event-dob-verification__actions-verify btn btn-primary" :value="$t('Verify')">
+        </div>
+      </div>
     </form>
   </div>
 </template>
@@ -43,14 +55,19 @@
             this.eventDetails = response.body;
           });
       },
-      next() {
-        this.isDobUnderage = UsersUtil.isUnderAge(this.date);
-        if (this.isDobUnderage) {
-          return;
+      async next() {
+        try {
+          await this.$validator.validateAll(); // Throws on invalid
+          this.isDobUnderage = UsersUtil.isUnderAge(this.date);
+          if (this.isDobUnderage) {
+            return;
+          }
+          StoreService.save('selected-event', this.eventDetails);
+          StoreService.save('applicant-dob', this.date);
+          this.$router.push({ name: 'EventSessions', params: { eventId: this.eventId } });
+        } catch (e) {
+          // Front-end validation is handling this
         }
-        StoreService.save('selected-event', this.eventDetails);
-        StoreService.save('applicant-dob', this.date);
-        this.$router.push({ name: 'EventSessions', params: { eventId: this.eventId } });
       },
     },
     created() {
@@ -59,6 +76,7 @@
   };
 </script>
 <style scoped lang="less">
+  @import "../common/variables";
   .cd-event-dob-verification {
     &__verify-age-message {
       display: block;
@@ -72,22 +90,38 @@
       margin-bottom: 8px;
       font-weight: normal;
     }
-    &__verify {
-      margin-top: 60px;
-      width: 197px;
-      height: 46px;
-      font-size: 16px;
-      font-weight: bold;
+    &__dob-info {
+      font-size: @font-size-medium;
     }
-    &__cancel {
-      margin-top: 60px;
-      margin-right: 18.9px;
-      width: 100px;
-      height: 46px;
-      color: #337ab7;
-      background-color: #ffffff;
-      font-size: 16px;
-      font-weight: bold;
+    &__invalid-dob-error {
+      display: block;
+    }
+    &__actions {
+      display: flex;
+      flex-wrap: wrap;
+      &-block {
+        margin-top: 60px;
+        display: inline-block;
+        &-errors {
+          margin-top: 35px;
+        }
+      }
+      &-verify {
+        width: 197px;
+        height: 46px;
+        font-size: 16px;
+        font-weight: bold;
+      }
+      &-cancel {
+        margin-top: 60px;
+        margin-right: 18.9px;
+        width: 100px;
+        height: 46px;
+        color: #337ab7;
+        background-color: #ffffff;
+        font-size: 16px;
+        font-weight: bold;
+      }
     }
     &__dob-picker-wrapper {
       max-width: 35%;
