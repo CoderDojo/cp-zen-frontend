cd427d43dff5837252357ae06e96d856d00e15cc
Wardormeur <wardormeur@gmail.com>
2018-01-11 16:46:34 +0000

    Add new languages

--- src/locale/cd-lang-picker.vue
+++ src/locale/cd-lang-picker.vue
@@ -3,7 +3,7 @@
     <div class="col-md-4 col-sm-6">
       <span class="fa fa-language fa-2x cd-lang-picker__icon"></span>
       <select class="cd-lang-picker__select" v-model="lang">
-        <option v-for="availableLanguage in availableLanguages" :value="availableLanguage.value">{{ availableLanguage.name }}</option>
+        <option v-for="availableLanguage in availableLanguages" :value="availableLanguage.code">{{ availableLanguage.name }} ({{ availableLanguage.country }})</option>
       </select>
     </div>
     <div class="col-md-8 col-sm-6">
@@ -16,97 +16,20 @@
   import { find } from 'lodash';
   import moment from 'moment';
   import Cookie from 'js-cookie';
+  import Vue from 'vue';
   import LocaleService from './service';
 
   export default {
     data() {
       return {
         lang: 'en_US', // default
-        availableLanguages: [
-          {
-            name: 'English (United States)',
-            value: 'en_US',
-          },
-          {
-            name: 'Nederlands (Nederland)',
-            value: 'nl_NL',
-          },
-          {
-            name: 'Deutsch (Deutschland)',
-            value: 'de_DE',
-          },
-          {
-            name: 'Italiano (Italia)',
-            value: 'it_IT',
-          },
-          {
-            name: 'Polski (Polska)',
-            value: 'pl_PL',
-          },
-          {
-            name: 'Português (Portugal)',
-            value: 'pt_PT',
-          },
-          {
-            name: 'Español (España)',
-            value: 'es_ES',
-          },
-          {
-            name: 'Türkçe (Türkiye)',
-            value: 'tr_TR',
-          },
-          {
-            name: 'Български (България)',
-            value: 'bg_BG',
-          },
-          {
-            name: 'Ελληνικά (Ελλάδα)',
-            value: 'el_GR',
-          },
-          {
-            name: 'Eesti (Eesti)',
-            value: 'et_EE',
-          },
-          {
-            name: 'हिंदी (इंडिया)',
-            value: 'hi_IN',
-          },
-          {
-            name: '日本語 (日本)',
-            value: 'ja_JP',
-          },
-          {
-            name: 'Română (România)',
-            value: 'ro_RO',
-          },
-          {
-            name: 'Español (Argentina)',
-            value: 'es_AR',
-          },
-          {
-            name: 'Français (France)',
-            value: 'fr_FR',
-          },
-          {
-            name: 'Maltese (Malta)',
-            value: 'mt_MT',
-          },
-          {
-            name: 'Slovenski (Slovenija)',
-            value: 'sl_SL',
-          },
-          {
-            name: 'Slovenský (Slovensko)',
-            value: 'sk_SK',
-          },
-          {
-            name: 'Українська (Україна)',
-            value: 'uk_UK',
-          },
-        ],
+        availableLanguages: [],
       };
     },
     methods: {
+      async getAvailableLanguages() {
+        return Vue.http.get(`${Vue.config.apiServer}/locale/languages`);
+      },
       setMomentLocale(locale) { // e.g. mt_MT
         const momentLocale = locale.replace('_', '-').toLowerCase(); // mt-mt
         const setMomentLocale = moment.locale(momentLocale); // zh-tw
@@ -132,11 +55,13 @@
           });
       },
     },
-    created() {
+    async created() {
+      this.availableLanguages = (await this.getAvailableLanguages()).body;
       const browserLocale = navigator.language.replace('-', '_');
       const matchingLocale = find(this.availableLanguages,
-        language => language.value === browserLocale);
+        language => language.code === browserLocale);
       const langCookie = Cookie.get('NG_TRANSLATE_LANG_KEY');
+      console.log('debug', langCookie, matchingLocale, browserLocale);
       if (langCookie) {
         this.lang = langCookie.substring(1, langCookie.length - 1);
       } else if (matchingLocale) {
