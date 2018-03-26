ad08515598c1edc94bb195defd23abaf0c33e4cd
Wardormeur <wardormeur@gmail.com>
2018-03-26 09:53:42 +0100

    Add actions, notes

--- src/events/cd-event-applications.vue
+++ src/events/cd-event-applications.vue
@@ -2,10 +2,13 @@
   <div class="row cd-event-applications">
     <div class="cd-">
       <header class="cd-event-applications__header">
-        <h3 class="cd-event-applications__header-title">
-          {{ event.name }}
-        </h3>
         <router-link :to="{ path: previousUrl }" replace class="cd-event-applications__header-old-interface">> Go back to previous interface</router-link>
+        <div class="cd-event-applications__header-row">
+          <h3 class="cd-event-applications__header-title">
+            {{ event.name }}
+          </h3>
+          <button class="btn btn-secondary"> <i class="fa fa-file-text-o"></i></button>
+        </div>
       </header>
     </div>
     <div id="people">
@@ -13,15 +16,40 @@
         <template slot="afterBody" scope="props">
           <!-- TODO : export -->
         </template>
-        <!-- TODO : mailTo -->
         <template slot="__group_meta" scope="{value, data}">
           <span class="cd-event-applications__grouping-header">({{ data.booked }}/{{ data.qty }})</span>
         </template>
         <template slot="status" scope="props">
           <input type="checkbox" v-model="props.row.isApproved"/> <!--@change="toggleApproval"> </input> -->
         </template>
-        <template slot="avatar" scope="props">
+        <template slot="name" scope="props">
           <img src="https://placebear.com/30/30"/>
+          <router-link :to="{ path: profileUrl(props.row.id) }">{{ props.row.name }}</router-link>
+        </template>
+        <template slot="notes" scope="props">
+          <i class="fa fa-commenting-o" :title="props.row.notes" v-if="isCustomNote(props.row.notes)"></i>
+        </template>
+        <template slot="appendBody" scope="props">
+          <tr>
+            <td colspan="3">
+              <div> {{ $t('Ninjas') }}: {{ nbNinja }} </div>
+            </td>
+            <td colspan="5">
+              <div> {{ $t('Mentors') }}: {{ nbMentor }} </div>
+            </td>
+          </tr>
+        </template>
+        <template slot="actions" scope="props">
+          <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" :id="`actions-${props.row.id}`" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="fa fa-ellipsis-v"></i>
+            </button>
+            <ul class="dropdown-menu" :aria-labelledby="`actions-${props.row.id}`">
+              <li><a href="#">Delete</a></li>
+              <!-- TODO : mailTo -->
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </div>
         </template>
         <!--<div slot="filter__sessionName">
           <input type="checkbox" class="form-control" v-model="tableOptions.groupBy" @change="toggleGrouping()">
@@ -64,6 +92,7 @@
             down: 'fa-sort-desc',
             is: 'fa-sort',
           },
+          sortable: [ 'name', 'parent', 'ticketName', 'ticketType', 'sessionName', 'status', 'created'],
           groupBy: this.groupBy,
           groupMeta: this.groupMeta,
           perPage: 999,
@@ -75,25 +104,30 @@
             sessionName: 'Session',
             created: 'Applied at',
             age: 'Age',
-            parent: 'Parent/Guardian',
+            parent: 'Parent',
             notes: 'Notes',
             status: 'Approved',
+            actions: '...',
           },
           highlightMatches: true,
           filterByColumn: true,
-          filterable: ['name', 'ticketName', 'ticketType', 'sessionName'],
+          filterable: ['name', 'parent', 'ticketName', 'ticketType', 'sessionName'],
           dateFormat: 'DD/MM HH:mm', // TODO i18n? moment?
           listColumns: this.listColumns, 
+          columnsClasses: {
+            'notes': 'cd-event-applications__notes-cell',
+          },
           descOrderColumn: 'applicationDate',
         }
       },
       tableData() {
         if(this.applications && this.applications.results) {
           return this.applications.results.map(a => {
-            const { name, dateOfBirth, notes, ticketName, ticketType, created, status } = a;
+            const { id, name, dateOfBirth, notes, ticketName, ticketType, created, status } = a;
             return Object.assign({
               sessionName: this.sessions[a.sessionId].name,
             }, {
+              id,
               name,
               age: UserUtils.getAge(moment.utc(a.dateOfBirth).toDate()),
               notes,
@@ -102,6 +136,7 @@
               notes,
               created: moment.utc(created),
               status,
+              notesVisible: false,
               isApproved: status === 'approved',
             });
           });
@@ -132,11 +167,12 @@
         return this.event.sessions.reduce(reducer, {});
       },
       tableColumns() {
-        let columns = ['avatar', 'name', 'parent', 'ticketName','ticketType', 'status', 'created', ]
+        let columns = ['name', 'parent', 'ticketName','ticketType', 'status', 'created', ]
         // TODO : do we need age, really ?
         //let columns = ['name', 'parent', 'ticketName','ticketType', 'status', 'age', 'created', ]
         if (this.hasCustomNotes) columns.splice(columns.length - 1, 0, 'notes');
         if (this.event && this.event.sessions && this.event.sessions.length > 1) columns.splice(3, 0, 'sessionName');
+        columns.push('actions');
         return columns;
       },
       listColumns() {
@@ -152,7 +188,7 @@
       hasCustomNotes() {
         const defaultNotes = i18n.t('N/A');
         if (this.applications && this.applications.results) {
-          return !this.applications.results.every(a => a.notes === defaultNotes);
+          return !this.applications.results.every((a) => !this.isCustomNote(a.notes));
         } 
         return true;
       },
@@ -165,6 +201,18 @@
       },
       previousUrl() {
         return `/dashboard/my-dojos/${this.dojoId}/events/${this.eventId}/applications`;
+      },
+      nbNinja() {
+        if (this.applications && this.applications.results) {
+          return (this.applications.results.filter(a => a.ticketType === 'ninja' && a.deleted === false && a.status === 'approved')).length;
+        }
+        return 0;
+      },
+      nbMentor() {
+        if (this.applications && this.applications.results) {
+          return (this.applications.results.filter(a => a.ticketType === 'mentor' && a.deleted === false && a.status === 'approved')).length;
+        }
+        return 0;
       },
     },
     methods: {
@@ -182,6 +230,14 @@
       },
       updateSelectableTickets(state) {
         this.filteredOnSession = state;
+      },
+      isCustomNote(note) {
+        const defaultNote = i18n.t('N/A');
+        const strippedNote = note.replace(/\s/g, ''); 
+        return !(strippedNote === '' || note === defaultNote);
+      },
+      profileUrl(id) {
+        return `/dashboard/profile/${id}`;
       },
     },
     components: {
@@ -216,10 +272,19 @@
     &__header{
       display: flex;
       justify-content: space-between;
+      flex-direction: column;
+      &-row {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        flex-direction: row;
+      }
       &-title {
         flex: 1;
       }
       &-old-interface {
+        display: block;
+        width: 100%;
         flex: 1;
         text-align: right;
       }
@@ -227,9 +292,19 @@
     &__grouping-header {
       float: right;
     }
-  }
-    tr {
-      font-weight: bold;
-      text-align: center;
+    &__notes {
+      &-cell {
+        text-align: center;
+      }
     }
+    &__status {
+      &-cell {
+        text-align: center;
+      }
+    }
+  }
+  tr {
+    font-weight: bold;
+    text-align: center;
+  }
 </style>
