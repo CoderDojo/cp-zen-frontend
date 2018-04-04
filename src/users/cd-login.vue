fb3f38d3c93061e81e14e35bbda7528707bc8634
Maimuna Ahmed <maimuna.p.ahmed@gmail.com>
2018-04-04 12:03:06 +0100

    Wrap strings for translations

--- src/users/cd-login.vue
+++ src/users/cd-login.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="cd-login container-fluid">
     <div class="row">
-      <h3 class="cd-login__header text-center">Login</h3>
+      <h3 class="cd-login__header text-center">{{ $t('Login') }}</h3>
     </div>
     <div class="row">
       <div class="cd-login__box">
         <form @submit.prevent="login">
           <div class="form-group">
-            <label>Email</label>
+            <label>{{ $t('Email') }}</label>
             <input class="form-control" data-vv-name="email" data-vv-validate-on="blur" type="email" v-model="email" v-validate="'required|email'" novalidate/>
           </div>
-          <p class="cd-login__email-req-err text-danger" v-show="errors.has('email:required')">{{ $t('Email address is required.') }}</p>
+          <p class="cd-login__email-req-err text-danger" v-show="errors.has('email:required')">{{ $t('Email is required') }}</p>
           <p class="cd-login__email-format-err text-danger" v-show="errors.has('email:email')">{{ $t('Email should be in the format: janedoe@example.com') }}</p>      
           <div class="form-group">
             <label>Password</label>
             <input class="form-control" data-vv-name="password" data-vv-validate-on="blur" type="password" v-model="password" v-validate="'required'" />
           </div>
-           <p class="cd-login__password-req-err text-danger" v-show="errors.has('password:required')">{{ $t('Password is required.') }}</p>
+           <p class="cd-login__password-req-err text-danger" v-show="errors.has('password:required')">{{ $t('Password is required') }}</p>
           <input class="cd-login__button btn btn-primary" type="submit" value="Login" />
         </form>
-        <p v-show="errors.has('loginFailed')" class="cd-login__login-failed text-danger">There was a problem logging in! Invalid email or password.</p>
-        <p class="cd-login__forgot-password"><a href="/reset">Forgot Password?</a></p>
-        <p class="cd-login__register">Don't have an account? <a href="/register">Register Here</a></p>
+        <p v-show="errors.has('loginFailed')" class="cd-login__login-failed text-danger">{{ $t('There was a problem logging in:') }} {{ $t('Invalid email or password!') }}</p>
+        <p class="cd-login__forgot-password"><a href="/reset">{{ $t('Forgot password?') }}</a></p>
+        <p class="cd-login__register">{{ $t("Don't have an account?") }} <a href="/register">{{ $t('Register Here') }}</a></p>
       </div>
     </div>
   </div>
