2fb02036ed2bf05d6f21d10b9ef0c36683f57917
Maimuna Ahmed <maimuna.p.ahmed@gmail.com>
2018-04-10 09:40:53 +0100

    unit tests for add child tickets element

---
+++ test/unit/specs/events/cd-event-add-child-tickets.spec.js
@@ -0,0 +1,311 @@
+import vueUnitHelper from 'vue-unit-helper';
+import ChildTicket from '!!vue-loader?inject!@/events/cd-event-add-child-ticket';
+
+describe('Add Child Ticket', () => {
+  let sandbox;
+  let MockVueDobPicker;
+  let MockVueMultiselect;
+  let ChildTicketWithMocks;
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+    MockVueDobPicker = {
+      dob: sandbox.stub(),
+    };
+    MockVueMultiselect = {
+      ticketSelect: sandbox.stub(),
+    };
+    ChildTicketWithMocks = ChildTicket({
+      'vue-dob-picker': MockVueDobPicker,
+      'vue-multiselect': MockVueMultiselect,
+    });
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('methods.showWhy()', () => {
+    it('should change the value of whyGender to true', async () => {
+      // ARRANGE
+      const vm = vueUnitHelper(ChildTicketWithMocks);
+      vm.whyGender = false;
+
+      // ACT
+      await vm.showWhy();
+
+      // ASSERT
+      expect(vm.whyGender).to.equal(true);
+    });
+  });
+
+  describe('methods.onTicketTouch()', () => {
+    it('should change the value of ticketTouch to true', async () => {
+      // ARRANGE
+      const vm = vueUnitHelper(ChildTicketWithMocks);
+      vm.ticketTouch = false;
+
+      // ACT
+      await vm.onTicketTouch();
+
+      // ASSERT
+      expect(vm.ticketTouch).to.equal(true);
+    });
+  });
+
+  describe('computed', () => {
+    describe('computed.childTickets', () => {
+      it('should filter out only child tickets from each sessions tickets', async () => {
+        // ARRANGE
+        const vm = vueUnitHelper(ChildTicketWithMocks);
+        vm.sessions = [{ description: 'description1',
+          entity$: 'sessionentity',
+          eventId: 'some-eventId',
+          id: 'sessionId',
+          name: 'Session1',
+          status: 'status1',
+          tickets: [
+            { approvedApplications: 0,
+              deleted: 0,
+              entity$: 'ticketentity',
+              id: 'ticketId1',
+              invites: null,
+              name: 'Ticket1',
+              quantity: 1,
+              sessionId: 'sessionId',
+              totalApplications: 0,
+              type: 'parent-guardian' },
+            { approvedApplications: 0,
+              deleted: 0,
+              entity$: 'ticketentity',
+              id: 'ticketId2',
+              invites: null,
+              name: 'Ticket2',
+              quantity: 1,
+              sessionId: 'sessionId',
+              totalApplications: 0,
+              type: 'ninja' }],
+        }];
+
+        // ACT
+
+        // ASSERT
+        expect(vm.childTickets).to.deep.equal([{ description: 'description1',
+          entity$: 'sessionentity',
+          eventId: 'some-eventId',
+          id: 'sessionId',
+          name: 'Session1',
+          status: 'status1',
+          tickets: [
+            { approvedApplications: 0,
+              deleted: 0,
+              entity$: 'ticketentity',
+              id: 'ticketId2',
+              invites: null,
+              name: 'Ticket2',
+              quantity: 1,
+              sessionId: 'sessionId',
+              totalApplications: 0,
+              type: 'ninja' }],
+        }]);
+      });
+    });
+
+    describe('computed.name', () => {
+      it('should return the full name - firstName and surname contactenated', async () => {
+        // ARRANGE
+        const vm = vueUnitHelper(ChildTicketWithMocks);
+        vm.firstName = 'Jane';
+        vm.surname = 'Doe';
+
+        // ACT
+
+        // ASSERT
+        expect(vm.name).to.equal('Jane Doe');
+      });
+    });
+
+    describe('computed.specifyGender', () => {
+      it('should return true if the selected gender has the value specify', async () => {
+        // ARRANGE
+        const vm = vueUnitHelper(ChildTicketWithMocks);
+        vm.genderSelect = 'specify';
+
+        // ACT
+
+        // ASSERT
+        expect(vm.specifyGender).to.equal(true);
+      });
+
+      it('should return false if the selected gender does not hav the value specify', async () => {
+        // ARRANGE
+        const vm = vueUnitHelper(ChildTicketWithMocks);
+        vm.genderSelect = 'male';
+
+        // ACT
+
+        // ASSERT
+        expect(vm.specifyGender).to.equal(false);
+      });
+    });
+
+    describe('computed.gender', () => {
+      it('should return the input gender value if the if the selected gender has the value specify', async () => {
+        // ARRANGE
+        const vm = vueUnitHelper(ChildTicketWithMocks);
+        vm.genderSelect = 'specify';
+        vm.genderInput = 'non-binary';
+
+        // ACT
+
+        // ASSERT
+        expect(vm.gender).to.equal('non-binary');
+      });
+
+      it('should return the selected gender value if the selected gender does not have the value specify', async () => {
+        // ARRANGE
+        const vm = vueUnitHelper(ChildTicketWithMocks);
+        vm.genderSelect = 'male';
+
+        // ACT
+
+        // ASSERT
+        expect(vm.gender).to.equal('male');
+      });
+    });
+
+    describe('computed.invalidTicket', () => {
+      it('should return the true if the ticketTouch is true and if the ticket length is equal to 0', async () => {
+        // ARRANGE
+        const vm = vueUnitHelper(ChildTicketWithMocks);
+        vm.ticketTouch = true;
+        vm.tickets = '';
+
+        // ACT
+
+        // ASSERT
+        expect(vm.invalidTicket).to.equal(true);
+      });
+
+      it('should return false if the ticketTouch is true and if the ticket length is not equal to 0', async () => {
+        // ARRANGE
+        const vm = vueUnitHelper(ChildTicketWithMocks);
+        vm.ticketTouch = true;
+        vm.tickets = [{
+          approvedApplications: 0,
+          deleted: 0,
+          entity$: 'ticketentity',
+          id: 'ticketId2',
+          invites: null,
+          name: 'Ticket2',
+          quantity: 1,
+          sessionId: 'sessionId',
+          totalApplications: 0,
+          type: 'ninja' }];
+
+        // ACT
+
+        // ASSERT
+        expect(vm.invalidTicket).to.equal(false);
+      });
+
+      it('should return false if the ticketTouch is false and if the ticket length is equal to 0', async () => {
+        // ARRANGE
+        const vm = vueUnitHelper(ChildTicketWithMocks);
+        vm.ticketTouch = false;
+        vm.tickets = '';
+
+        // ACT
+
+        // ASSERT
+        expect(vm.invalidTicket).to.equal(false);
+      });
+
+
+      it('should return false if the ticketTouch is false and if the ticket length is not equal to 0', async () => {
+        // ARRANGE
+        const vm = vueUnitHelper(ChildTicketWithMocks);
+        vm.ticketTouch = false;
+        vm.tickets = [{
+          approvedApplications: 0,
+          deleted: 0,
+          entity$: 'ticketentity',
+          id: 'ticketId2',
+          invites: null,
+          name: 'Ticket2',
+          quantity: 1,
+          sessionId: 'sessionId',
+          totalApplications: 0,
+          type: 'ninja' }];
+
+        // ACT
+
+        // ASSERT
+        expect(vm.invalidTicket).to.equal(false);
+      });
+    });
+
+    describe('computed.status', () => {
+      it('should return a status of pending if the event ticketApproval is set to true', async () => {
+        // ARRANGE
+        const vm = vueUnitHelper(ChildTicketWithMocks);
+        vm.event = { ticketApproval: true };
+
+        // ACT
+
+        // ASSERT
+        expect(vm.status).to.equal('pending');
+      });
+
+      it('should return a status of approved if the event ticketApproval is set to false', async () => {
+        // ARRANGE
+        const vm = vueUnitHelper(ChildTicketWithMocks);
+        vm.event = { ticketApproval: false };
+
+        // ACT
+
+        // ASSERT
+        expect(vm.status).to.equal('approved');
+      });
+    });
+
+    describe('computed.applications', () => {
+      it('should return full application when form is filled out', async () => {
+        // ARRANGE
+        const vm = vueUnitHelper(ChildTicketWithMocks);
+        vm.name = 'Jane Doe';
+        vm.dob = new Date(1980, 10, 25, 0, 0, 0, 0);
+        vm.eventId = 'eventId';
+        vm.status = 'pending';
+        vm.tickets = [{
+          approvedApplications: 0,
+          deleted: 0,
+          entity$: 'ticketentity',
+          id: 'ticketId2',
+          invites: null,
+          name: 'Ticket2',
+          quantity: 1,
+          sessionId: 'sessionId',
+          totalApplications: 0,
+          type: 'ninja' }];
+        vm.event = { dojoId: 'dojoId' };
+
+        // ACT
+
+        // ASSERT
+        expect(vm.applications).to.deep.equal([{
+          created: new Date(),
+          date_of_birth: new Date(1980, 10, 25, 0, 0, 0, 0),
+          dojo_id: 'dojoId',
+          event_id: 'eventId',
+          name: 'Jane Doe',
+          session_id: 'sessionId',
+          status: 'pending',
+          ticket_id: 'ticketId2',
+          ticket_name: 'Ticket2',
+          ticket_type: 'ninja',
+        }]);
+      });
+    });
+  });
+});
