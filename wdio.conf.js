114c9573d6e404419695ffa104ada81aa67b10de
Wardormeur <wardormeur@gmail.com>
2018-04-25 09:20:32 +0100

    E2e tests for docker (#112)
    * Remove wdio-allure-reporter
    
    * Add missing yarn lock

--- wdio.conf.js
+++ wdio.conf.js
@@ -122,7 +122,7 @@ exports.config = {
   // Test reporter for stdout.
   // The only one supported by default is 'dot'
   // see also: http://webdriver.io/guide/testrunner/reporters.html
-  reporters: ['spec', 'allure'],
+  reporters: ['spec'],
 
   //
   // Options to be passed to Mocha.
