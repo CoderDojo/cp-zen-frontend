a5a9973505fd1ee7841192c8232e37d000f959a1
Maimuna Ahmed <maimuna.p.ahmed@gmail.com>
2018-08-27 17:42:49 +0100

    rebase off #173
    show title, date, and post type
    
    vertically align title
    
    sort all posts by date

--- src/dashboard/cd-dashboard-news.vue
+++ src/dashboard/cd-dashboard-news.vue
@@ -1,14 +1,82 @@
 <template>
   <div class="column">
     <div class="cd-dashboard-news">
-      <h3 class="cd-dashboard-news__header">Community News and Forum Updates</h3>
+      <h2 class="cd-dashboard-news__header">Community News and Forum Updates</h2>
+      <div class="cd-dashboard-news__posts" v-for="post in allUpdates">
+        <span class="cd-dashboard-news__posts-left">
+          <p class="cd-dashboard-news__post-type">{{ post.type }}</p>
+          <p class="cd-dashboard-news__post-date">{{ post.date }}</p>
+        </span>
+        <span class="cd-dashboard-news__posts-right">
+          <h4 class="cd-dashboard-news__post-title">
+            <a class="cd-dashboard-news__post-title-link" :href="`${post.link}`">{{ post.title }}</a>
+          </h4>
+        </span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+  import NewsForumsService from './service';
+
   export default {
     name: 'cd-dashboard-news',
+    data() {
+      return {
+        news: null,
+        forums: null,
+      };
+    },
+    computed: {
+      allUpdates() {
+        if (this.news && this.forums) {
+          const joinedPosts = [...this.formattedNews, ...this.formattedForums];
+          return this.sortPostsByDate(joinedPosts).splice(0, 6);
+        }
+        return null;
+      },
+      formattedNews() {
+        return (this.news).map(post => ({
+          type: 'News',
+          date: post.date,
+          link: post.link,
+          title: post.title.rendered,
+        }));
+      },
+      formattedForums() {
+        return (this.forums).map(post => ({
+          type: 'Forums',
+          date: post.timestampISO,
+          link: `https://forums.coderdojo.com/topic/${post.slug}`,
+          title: post.title,
+        }));
+      },
+    },
+    methods: {
+      async loadNews() {
+        const res = await NewsForumsService.loadNews();
+        this.news = res.body;
+      },
+      async loadForums() {
+        const res = await NewsForumsService.loadForums();
+        this.forums = res.body.topics;
+      },
+      sortPostsByDate(posts) {
+        const sortedPosts = posts.sort((a, b) =>
+         a.date - b.date);
+        return (sortedPosts.map(post => (Object.assign({
+          type: post.type,
+          date: [post.date.slice(0, 4), post.date.slice(4, 6), post.date.slice(6, 8)].reverse().join('/'),
+          link: post.link,
+          title: post.title,
+        }))));
+      },
+    },
+    async created() {
+      this.loadNews();
+      this.loadForums();
+    },
   };
 </script>
 
@@ -19,13 +87,54 @@
 
   .cd-dashboard-news {
     background-color: #f4f5f6;
-    padding: 0 32px;
+    padding: 0 32px 45px;
     min-height: 432px;
     width: 940px;
     display: flex;
+    flex-direction: column;
 
     &__header {
       padding: 16px;
     }
+
+    &__posts {
+      margin: 16px 0;
+      display: flex;
+      flex-direction: row;
+      max-width: 75%;
+
+      &-left {
+        flex-direction: column;
+        margin: 0 16px 0 16px;
+        max-width: 30%;
+      }
+
+      &-right{
+        margin: 0 16px 0 16px;
+        max-width: 70%;
+      }
+    }
+
+    &__post {
+      &-type {
+        font-weight: bold;
+      }
+
+      &-date {
+        color: #7b8082;
+      }
+
+      &-title {
+        margin: 0;
+
+        &-link {
+          color: @cd-purple;
+          &:hover {
+            color: #a57ec7;
+          }
+        }
+      }
+    }
+
   }
 </style>
