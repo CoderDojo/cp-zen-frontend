58c991537835e1472286ec7a1cb62c459dff54f8
Maimuna Ahmed <heyitsmaimai@users.noreply.github.com>
2018-08-28 10:42:54 +0100

    Merge branch with Master (Dashboard children/show children (#168))
    * Display children. Set header margins for each page
    
    * add unit tests for dashboard children component so far.change some variable names
    
    * fix unit tests and add translations
    
    * fix child name. use vuex store for the current user
    
    * fix unit tests
    
    * get rid of overall flex and column for children component

--- src/dashboard/cd-dashboard-children.vue
+++ src/dashboard/cd-dashboard-children.vue
@@ -1,16 +1,55 @@
 <template>
   <div class="column">
     <div class="cd-dashboard-children">
-      <h1 class="cd-dashboard-children__header">My Kids</h1>
+      <h1 class="cd-dashboard-children__header">{{ $t('My Children') }}</h1>
+      <div class="cd-dashboard-children__child" v-for="child in children">
+        <h3 class="cd-dashboard-children__name">
+          {{ child.name }}
+          <a :href="`/dashboard/profile/${userId}/edit`" class="cd-dashboard-children__edit-child"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+        </h3>
+      </div>
     </div>
   </div>
 </div>
 </template>
 
 <script>
+  import UserService from '@/users/service';
+  import { mapGetters } from 'vuex';
+
   export default {
     name: 'cd-dashboard-children',
-    components: {
+    data() {
+      return {
+        userProfile: {},
+        userChildren: [],
+      };
+    },
+    computed: {
+      ...mapGetters(['loggedInUser']),
+      children() {
+        if (this.userChildren) {
+          return this.userChildren;
+        }
+        return null;
+      },
+    },
+    methods: {
+      async loadProfile() {
+        this.userProfile = (await UserService.userProfileData(this.loggedInUser.id)).body;
+      },
+      async loadChildren() {
+        if (this.userProfile.children) {
+          this.userChildren = (await Promise.all(
+            this.userProfile.children.map(
+              child => UserService.userProfileData(child))))
+            .map(res => res.body);
+        }
+      },
+    },
+    async created() {
+      await this.loadProfile();
+      this.loadChildren();
     },
   };
 </script>
@@ -23,10 +62,26 @@
     padding: 0 32px;
     margin-left: auto;
     min-height: 100%;
-    display:flex;
 
     &__header {
-      padding: 16px;
+      margin: 45px 0 16px 0;
+    }
+
+    &__child {
+      margin: 16px 0 16px 0;
+      display: flex;
+      flex-direction: column;
+    }
+
+    &__name {
+      display: flex;
+    }
+
+    &__edit-child {
+      font-size: 14px;
+      display: flex;
+      align-self: flex-end;
+      padding: 0 0 0 5px;
     }
   }
 </style>
