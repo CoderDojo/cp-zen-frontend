57070b9ee7e74e426ff06ecfc15acbd0c3f6db86
Wardormeur <wardormeur@gmail.com>
2018-09-04 11:55:12 +0100

    Add e2e tests for dashboard badges display (#177)

---
+++ cypress/pages/home.js
@@ -0,0 +1,6 @@
+export default {
+  childrenTitle: '.cd-dashboard-children__header',
+  children: '.cd-dashboard-children .cd-dashboard-children__child',
+  childrenNames: '.cd-dashboard-children__name',
+  childrenBadges: '.cd-dashboard-children__badge',
+};
