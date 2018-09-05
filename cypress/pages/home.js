ddf45bbe764e0caa851f69dae4d00b1117a2f513
Wardormeur <wardormeur@gmail.com>
2018-09-05 10:50:51 +0100

    Add e2e tests for dashboard news display (#179)
    * Add e2e tests for dashboard news display
    
    * Fix flakyness from children badges tests
    
    * Move blogArticles to json format

--- cypress/pages/home.js
+++ cypress/pages/home.js
@@ -3,4 +3,9 @@ export default {
   children: '.cd-dashboard-children .cd-dashboard-children__child',
   childrenNames: '.cd-dashboard-children__name',
   childrenBadges: '.cd-dashboard-children__badge',
+  newsTitle: '.cd-dashboard-news__header',
+  newsEntries: '.cd-dashboard-news__posts',
+  newsEntryTitle: '.cd-dashboard-news__post-title',
+  newsEntryDate : '.cd-dashboard-news__post-date',
+  newsEntryCategory: '.cd-dashboard-news__post-type',
 };
