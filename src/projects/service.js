780260d6ff2c130ecb303d18514c651c5e115853
Wardormeur <wardormeur@gmail.com>
2018-09-06 10:30:48 +0100

    Remove eslint disabling, idk what I was on

--- src/projects/service.js
+++ src/projects/service.js
@@ -3,7 +3,6 @@ import Vue from 'vue';
 const ProjectsService = {
   list: (language, params) => {
     // Note : this is a patch due to /projects non-consistent language
-    // eslint-disable-next-line no-unused-vars
     const lang = language.toLowerCase() === 'en_us' ? 'en' : language.toLowerCase();
     return Vue.http.get(`${Vue.config.projectsUrlBase}/api/v1/${lang}/projects`, { params });
   },
