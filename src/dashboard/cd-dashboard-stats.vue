2e0142c3119faee02cb9096609d6451abfa0ec08
Wardormeur <wardormeur@gmail.com>
2018-09-10 09:49:27 +0100

    Dashboard/stats number kids (#189)
    * Display the number of applications done for 1 dojo
    
    * Add unit tests
    
    * Fix linting
    
    * Restrict set up of dojoId to champion relationship
    
    * Remove uneeded code

--- src/dashboard/cd-dashboard-stats.vue
+++ src/dashboard/cd-dashboard-stats.vue
@@ -1,16 +1,50 @@
 <template>
   <div class="column">
     <div class="cd-dashboard-stats">
-      <h1 class="cd-dashboard-stats__header">Dojo Stats</h1>
+      <h1 class="cd-dashboard-stats__header">{{ $t('Dojo Stats') }}</h1>
+      <div class="cd-dashboard-stats__number">
+        <div class="cd-dashboard-stats__number-value"><span>{{ bookedChildren }}</span></div>
+        <div class="cd-dashboard-stats__description">{{ $t('kids attended your events') }}</div>
+      </div>
     </div>
   </div>
 </div>
 </template>
 
 <script>
+  import { mapGetters } from 'vuex';
+  import DojoService from '@/dojos/service';
+  import EventsService from '@/events/service';
+  
   export default {
     name: 'cd-dashboard-stats',
-    components: {
+    data() {
+      return {
+        dojos: null,
+        dojoId: null,
+        bookedChildren: 0,
+      };
+    },
+    computed: {
+      ...mapGetters(['loggedInUser']),
+    },
+    methods: {
+      async getDojos() {
+        const dojos = (await DojoService.getUsersDojos(this.loggedInUser.id)).body;
+        this.dojos = dojos.filter(dojo => dojo.userTypes.includes('champion'));
+        // TODO : drop-down dojo selection
+        this.dojoId = this.dojos.length ? this.dojos[0].dojoId : null;
+      },
+      async getBookedChildren() {
+        this.bookedChildren = (await EventsService.searchApplicationsByDojo(
+          this.dojoId,
+          { deleted: 0, ticketType: 'ninja', status: { ne$: 'cancelled' } },
+        )).body.length;
+      },
+    },
+    async created() {
+      await this.getDojos();
+      await this.getBookedChildren();
     },
   };
 </script>
@@ -25,10 +59,19 @@
     margin-left: auto;
     min-height: 100%;
     max-width: 340px;
-    display:flex;
+    display: block; 
 
     &__header {
       margin: 45px 0 16px 0;
+    }
+    &__number {
+      text-align: center;
+      &-value {
+        span {
+          padding: 8px;
+        }
+        font-size: @font-size-h3;  
+      }
     }
   }
 
