9b365c33074e0c744d55fa794bf32a36582aa060
Wardormeur <wardormeur@gmail.com>
2018-09-11 15:53:57 +0100

    Make tests pass in CI

--- cypress/integration/dashboard/stats_spec.js
+++ cypress/integration/dashboard/stats_spec.js
@@ -17,9 +17,12 @@ describe('Homepage stats', () => {
   it('should show a message pushing champions to use Zen when no stats is available', () => {
     cy.route('/api/2.0/users/instance', 'fx:parentLoggedIn').as('loggedIn');
     cy.route('POST', '/api/2.0/dojos/load-dojo-users', { response: [] }).as('dojoUsers');
+    cy.route('POST', '/api/2.0/events/applications/search', []).as('bookedChildren');
     cy.visit('/home');
     cy.wait('@loggedIn');
     cy.wait('@userDojos');
+    cy.wait('@bookedChildren');
+    cy.wait('@dojoUsers');
     cy.wait('@dojoUsers');
     cy.get(homePage.statsUseZenMessage).invoke('text').should('eq', "No statistics are available at the moment.\n        The more Zen is used, the more you\'ll find about your Dojo!");
   });
@@ -31,13 +34,17 @@ describe('Homepage stats', () => {
         { gender: 'Male' },
         { gender: 'Male' }
       ] }).as('dojoUsers');
+      cy.route('POST', '/api/2.0/events/applications/search', []).as('bookedChildren');;
       cy.visit('/home');
       cy.wait('@loggedIn');
+      cy.wait('@bookedChildren');
+      cy.wait('@userDojos');
       cy.wait('@dojoUsers');
       cy.get(homePage.statsNbYouthChart).invoke('text').should('eq', '{numKids} kids attended your events');
     });
     it('should show the stats chart of youth gender', () => {
       cy.route('/api/2.0/users/instance', 'fx:parentLoggedIn').as('loggedIn');
+      cy.route('POST', '/api/2.0/events/applications/search', []).as('bookedChildren');;
       cy.route('POST', '/api/2.0/dojos/load-dojo-users', { response: [
         { gender: 'Female'},
         { gender: 'Male' },
@@ -45,6 +52,8 @@ describe('Homepage stats', () => {
       ] }).as('dojoUsers');
       cy.visit('/home');
       cy.wait('@loggedIn');
+      cy.wait('@bookedChildren');
+      cy.wait('@userDojos');
       cy.wait('@dojoUsers');
       cy.get(homePage.statsYouthGenderChart).find('svg').should('be.visible');
     });
