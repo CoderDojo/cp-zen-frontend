27c3ac9c8d1598b3ba9b81a8d83f5071486abc95
Daniel Brierton <daniel@brierton.me>
2018-09-11 11:06:34 +0100

    Fix prod env vars
    Missing quotes 👍

--- config/prod.env.js
+++ config/prod.env.js
@@ -5,7 +5,7 @@ module.exports = {
   GOOGLE_ANALYTICS_PROPERTY_ID: process.env.GIT_BRANCH === 'master' ? '"UA-25136319-2"' : '"UA-25136319-8"',
   GOOGLE_MAPS_API_KEY: '"AIzaSyC3xF9XV91bS2R14Gjmx3UQaKbGgAfHbE4"',
   GIT_BRANCH: `"${process.env.GIT_BRANCH}"`,
-  NEWS_URL_BASE: 'https://coderdojo.com',
-  FORUMS_URL_BASE: 'https://forums.coderdojo.com',
+  NEWS_URL_BASE: '"https://coderdojo.com"',
+  FORUMS_URL_BASE: '"https://forums.coderdojo.com"',
   PROJECTS_URL_BASE: '"https://learning-admin.raspberrypi.org"',
 }
