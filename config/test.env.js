83d003cd829d14b076a2e1cd98ec2d50670e73e8
Wardormeur <wardormeur@gmail.com>
2019-02-08 16:10:16 +0000

    Fix CCI issue by supporting Chrome 72 (#237)
    * Fix CCI issue by supporting Chrome 72
    
    * Renew the GAPI dev key
    
    * Random fixes when adding the dashboard
    comment tests which are failing for unknown reason, as this is not supposed to be maitained anyway
    
    * Bye old e2e tests, I had enough

--- config/test.env.js
+++ config/test.env.js
@@ -5,6 +5,6 @@ module.exports = merge(devEnv, {
   NODE_ENV: '"testing"',
   API_SERVER: '""',
   GOOGLE_ANALYTICS_PROPERTY_ID: '"UA-25136319-8"',
-  GOOGLE_MAPS_API_KEY: '"AIzaSyC3xF9XV91bS2R14Gjmx3UQaKbGgAfHbE4"',
+  GOOGLE_MAPS_API_KEY: '"AIzaSyBerDEIeMqY0gxKudojYwziBQhmHrXVMCI"',
   FORUMS_URL_BASE: '"http://localhost:4567"',
 })
