cdab1741b9046928dfb4ff5b03140d67563cb9a3
Wardormeur <wardormeur@gmail.com>
2019-03-05 00:33:58 +0000

    Fix linting

--- test/unit/specs/events/cd-ics-link.spec.js
+++ test/unit/specs/events/cd-ics-link.spec.js
@@ -1,7 +1,6 @@
 import vueUnitHelper from 'vue-unit-helper';
 import IcsLink from '@/events/cd-ics-link';
 import TimeShift from 'timeshift-js';
-import moment from 'moment';
 
 describe('ICS link component', () => {
   describe('computed', () => {
@@ -11,7 +10,7 @@ describe('ICS link component', () => {
         Date = TimeShift.Date; // eslint-disable-line no-global-assign
         TimeShift.setTimezoneOffset(-120); // UTC+2 (Italy, summer time)
         TimeShift.setTime(1501079400000); // 2017-07-26 16:30:00 GMT+02:00
-        vm.dojoId = 1; 
+        vm.dojoId = 1;
 
         expect(vm.url).to.match(/\/api\/3\.0\/dojos\/1\/events\.ics\?query\[status\]=published&query\[afterDate\]=1501079400&query\[utcOffset\]=120&query\[zone\]=.+\/.+/);
       });
