b12bd7af4bf9c46c74226ece2212155a842defc2
Ant Barnes <ant@arayb.com>
2019-06-10 13:30:16 +0100

    Ensure en_US locale returns projects
    When the users cookie locale is en_US, or presumably any other en_
    locale we were sending the entire locale to learning admin to get
    projects back.
    Learning admin expects the locale to be 'en' for english.

--- src/dashboard/cd-dashboard-projects.vue
+++ src/dashboard/cd-dashboard-projects.vue
@@ -4,7 +4,7 @@
     <h2 class="cd-dashboard-projects__header visible-xs">{{ $t('Here are some projects you can try') }}</h2>
     <hr class ="cd-dashboard-projects__divider visible-xs">
     <div v-show="isDisplayable" class="cd-dashboard-projects__cards">
-      <a class="cd-dashboard-projects__card" v-for="project in projects" :key="project.id" 
+      <a class="cd-dashboard-projects__card" v-for="project in projects" :key="project.id"
         :href="`https://projects.raspberrypi.org/${locale}/projects/${project.attributes.repositoryName}`"
         v-ga-track-exit-nav>
         <img :src="project.attributes.content.heroImage" />
@@ -41,7 +41,11 @@
       userLocale() {
         const langCookie = LocaleService.getUserLocale();
         if (langCookie) {
-          return langCookie.replace(/"/g, '').replace('_', '-');
+          let formattedLocale = langCookie.replace(/"/g, '').replace('_', '-');
+          if (/en-.*/.test(formattedLocale)) {
+            formattedLocale = 'en';
+          }
+          return formattedLocale;
         }
         return 'en';
       },
