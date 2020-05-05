16a6c664103b95b69e04b69034c9cbd1538b9bf6
Patrick J Cherry <patch@dominoid.net>
2020-05-05 15:17:55 +0100

    Change Raspberry Pi community to CoderDojo

--- src/common/cd-covid-banner.vue
+++ src/common/cd-covid-banner.vue
@@ -5,7 +5,7 @@
       <p>{{ $t('In the meantime, we’re providing exciting opportunities for young people, parents, volunteers, and educators to get creative with tech through') }}
         <a class="cd-banner-link" target="_blank" href="https://www.raspberrypi.org/learn/?utm_source=coderdojo&utm_medium=covid-banner&utm_campaign=dmah">{{ $t('Digital Making at Home') }}</a> {{ $t('from the Raspberry Pi Foundation.') }}
       </p>
-      <p>{{ $t('There are still many ways for you to get involved in the Raspberry Pi community!') }}
+      <p>{{ $t('There are still many ways for you to get involved in the CoderDojo community!') }}
         <a class="cd-banner-link" href="https://zen.coderdojo.com/dashboard/start-dojo">{{ $t('Please register your details') }}</a> {{ $t('with us so we can find out how best to support you.') }}
       </p>
     </div>
