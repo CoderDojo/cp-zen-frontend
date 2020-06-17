9c237d7fe8750150c26c64c5efe1b5bb8156805f
Patrick J Cherry <patch@dominoid.net>
2020-06-17 17:31:10 +0100

    HTML escape ampersands in URL

--- src/common/cd-covid-banner.vue
+++ src/common/cd-covid-banner.vue
@@ -13,7 +13,7 @@
       <p>
         {{ $t('If you’re not able to start your Dojo in person yet, there are many other ways that you can get involved in the meantime.') }}
         {{ $t('Check out the exciting opportunities for young people, parents, volunteers, and educators to learn and get creative with tech through') }}
-        <a class="cd-banner-link" target="_blank" href="https://www.raspberrypi.org/learn/?utm_source=coderdojo&utm_medium=covid-banner&utm_campaign=dmah">{{ $t('Digital Making at Home') }}</a>
+        <a class="cd-banner-link" target="_blank" href="https://www.raspberrypi.org/learn/?utm_source=coderdojo&amp;utm_medium=covid-banner&amp;utm_campaign=dmah">{{ $t('Digital Making at Home') }}</a>
         {{ $t('from the Raspberry Pi Foundation.') }}
       </p>
     </div>
