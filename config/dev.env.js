43eb13af0366c7cf478decccfb19abfd09250af6
Patrick J. Cherry <patch@dominoid.net>
2021-12-24 09:47:48 +0000

    Update Google Maps API key

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,7 +6,7 @@ module.exports = merge(prodEnv, {
   API_SERVER: '""', // False good idea, remove me
   RECAPTCHA_SITE_KEY: '"6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"',
   GOOGLE_ANALYTICS_PROPERTY_ID: '"UA-25136319-8"',
-  GOOGLE_MAPS_API_KEY: '"AIzaSyD6WWeWYAVLLO1rmVSLiK-gSQUK8-U6w7w"',
+  GOOGLE_MAPS_API_KEY: '"AIzaSyA4lRJMDmdT_60aUSsih78adO2A9Gzv5UM"',
   NEWS_URL_BASE: '""',
   FORUMS_URL_BASE: '"http://localhost:4567"',
   PROJECTS_URL_BASE: '""',
